c899500d55e6e6de1eb52b7c56ef52d2eed70178
Seasonbrucelee <sezen39@hotmail.com>
2022-01-17 16:35:12 +0100

    javascript lundi 17.01.2022
    javascript

--- js/main.js
+++ js/main.js
@@ -1,5 +1,17 @@
-//alert('Hello World !');
-//document.open();
+/*alert('Hello World !');
+document.open();*/
 //document.write('Bonjour le monde!');
-document.write('<p class="para">Bonjour le monde!</p>');
+//document.write('<p class="para">Bonjour le monde!</p>');
 //document.close();
+/*let start = "Bonjour ",
+    prenom,
+    end = '!',
+    result;
+prenom = prompt('Quel est votre prénom?');
+result = start + prenom + end;
+alert(result);
+
+console.log(result);
+document.open();
+document.write("<div id='monid'>" + result + "</div>");
+document.close();*/
